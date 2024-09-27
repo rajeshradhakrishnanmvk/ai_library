25e0b16b8563ee4c646053112584399ef5c1e6b1
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2024-09-27 15:20:53 +0530

    json serialize the Book Object

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,13 +1,35 @@
 namespace BooksApi.Models;
 
+// public class Book
+// {
+
+//     public int BookId { get; set; }
+//     public string? Name { get; set; }
+//     public string? Author { get; set; }
+//     public string? Description { get; set; }
+
+//     public string? Library { get; set; }
+
+// }
+
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 public class Book
 {
-
+    [JsonPropertyName("bookId")]
     public int BookId { get; set; }
+
+    [JsonPropertyName("name")]
     public string? Name { get; set; }
+
+    [JsonPropertyName("author")]
     public string? Author { get; set; }
+
+    [JsonPropertyName("description")]
     public string? Description { get; set; }
 
+    [JsonPropertyName("library")]
     public string? Library { get; set; }
-
 }
